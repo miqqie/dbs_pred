47570c2c769a8ece3db5b1de379f9fde3f59a814
miqqie <awmeiqin@gmail.com>
2025-07-14 07:58:57 +0000

    updated app.py and html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 
 from flask import Flask, render_template, request
+from groq import Groq
 import joblib
+import os
+
+#os.environ[''] = ""
 
 app = Flask(__name__)
 
@@ -14,9 +18,29 @@ def main():
     # db
     return(render_template("main.html"))
 
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
 @app.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {
+            "role": "user",
+            "content": q
+        }
+    ]
+)
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
 
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
