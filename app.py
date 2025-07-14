8f5f3f880c63d70b05ed741a84cbdf1b7e44a586
miqqie <awmeiqin@gmail.com>
2025-07-14 06:49:27 +0000

    updated htmls and app.py

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-#basic structure of flask app
 
 from flask import Flask, render_template, request
 import joblib
 
 app = Flask(__name__)
-#app corresponds to name of py file. app > app.py
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    # db
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-
     # load model
     model = joblib.load("dbs.jl")
-
     # make prediction
     pred = model.predict([[q]])
-
     return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
     app.run()
+
